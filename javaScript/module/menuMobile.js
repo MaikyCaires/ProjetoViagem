0efb41d676f819bd59209059bcef955755d9d5ee
Maiky Caires <122680022+MaikyCaires@users.noreply.github.com>
2024-05-14 14:58:23 -0300

    Update menuMobile.js

--- javaScript/module/menuMobile.js
+++ javaScript/module/menuMobile.js
@@ -1,14 +1,22 @@
-export default function menuMobile(){
+export default function menuMobile() {
+  const btn = document.querySelector('[data-mobile="menu"]');
+  const menu = document.querySelector('[data-menu="main"]');
+  const container = document.querySelector('[data-container="container"]');
 
+  function mostrar(event) {
+    menu.classList.toggle("mostrar-menu");
+    if (menu.classList.contains("mostrar-menu")) {
+      container.addEventListener("click", clicarFora);
+    } else {
+      container.removeEventListener("click", clicarFora);
+    }
+  }
+
+  function clicarFora(event) {
+    if (event.target != btn) {
+      menu.classList.remove("mostrar-menu");
+    }
+  }
+
+  btn.addEventListener("click", mostrar);
 }
-
-const btn = document.querySelector('[data-mobile="menu"]')
-const menu = document.querySelector('[data-menu="main"]')
-
-
-function mostrar(event){
-    menu.classList.toggle("mostrar-menu")
-}
-
-
-btn.addEventListener("click", mostrar)
