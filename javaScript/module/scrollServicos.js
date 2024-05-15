5a49c4afa24990d7797b6a1a7d92f593a313af57
Maiky Caires <122680022+MaikyCaires@users.noreply.github.com>
2024-05-15 14:49:12 -0300

    Update scrollServicos.js

--- javaScript/module/scrollServicos.js
+++ javaScript/module/scrollServicos.js
@@ -1,23 +1,33 @@
 export default function mostrarServico() {
-  const container = document.querySelector('[data-container="servicos"]');
-
-  const card = document.querySelectorAll('[data-card]');
-
-
-  let alturaDisplay = window.innerHeight * 0.6;
-  console.log(alturaDisplay);
-
-  function mostrar(event) {
-    let rect = container.getBoundingClientRect().top;
-    if (rect <= alturaDisplay) {
-      card.forEach((elemento, indice) =>{
-        setTimeout(() =>{
-            elemento.classList.add("mostrarServico")
-        }, indice * 500)
-      })
+    const container = document.querySelector('[data-container="servicos"]');
+    const card = document.querySelectorAll('[data-card]');
+    let alturaDisplay = 0;
+    
+    function ajustarAltura() {
+        if (window.innerWidth <= 800) { 
+            alturaDisplay = window.innerHeight * 0.4; 
+        } else {
+            alturaDisplay = window.innerHeight * 0.6; 
+        }
     }
-  }
-  mostrar();
+    
+    function mostrar(event) {
+        ajustarAltura(); // Atualiza a altura de exibição antes de verificar a visibilidade do contêiner
+        let rect = container.getBoundingClientRect().top;
+        if (rect <= alturaDisplay) {
+            card.forEach((elemento, indice) => {
+                setTimeout(() => {
+                    elemento.classList.add("mostrarServico")
+                }, indice * 500)
+            })
+        }
+    }
+    
+    mostrar();
+    window.addEventListener("resize", ajustarAltura); // Atualiza alturaDisplay quando a janela é redimensionada
+    window.addEventListener("scroll", mostrar);
+    
 
-  window.addEventListener("scroll", mostrar);
+  
+  
 }
