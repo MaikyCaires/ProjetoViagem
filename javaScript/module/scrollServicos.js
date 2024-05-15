bc50c6d769df24c1905f947e3b5a31bb7d7ebe06
Maiky Caires <122680022+MaikyCaires@users.noreply.github.com>
2024-05-15 15:41:12 -0300

    adicionando animação

--- javaScript/module/scrollServicos.js
+++ javaScript/module/scrollServicos.js
@@ -1,11 +1,12 @@
 export default function mostrarServico() {
     const container = document.querySelector('[data-container="servicos"]');
     const card = document.querySelectorAll('[data-card]');
+    const card1 = document.querySelector('[data-card]');
     let alturaDisplay = 0;
     
     function ajustarAltura() {
         if (window.innerWidth <= 800) { 
-            alturaDisplay = window.innerHeight * 0.2; 
+            alturaDisplay = window.innerHeight * 0.3    ; 
         } else {
             alturaDisplay = window.innerHeight * 0.6; 
         }
@@ -22,10 +23,33 @@ export default function mostrarServico() {
             })
         }
     }
+
+    function hover(event){
+        let alturaMobile = window.innerHeight * 0.5 ;
+        card.forEach(cardElemento =>{
+            let rectCard = cardElemento.getBoundingClientRect().top
+
+            if(rectCard <= alturaMobile){
+                cardElemento.classList.add("ativoService")
+            }else{
+                cardElemento.classList.remove("ativoService")
+            }
+        })
+        
+        
+       
+        
+       
+    }
     
+
+
     mostrar();
     window.addEventListener("resize", ajustarAltura); // Atualiza alturaDisplay quando a janela é redimensionada
-    window.addEventListener("scroll", mostrar);
+    window.addEventListener("scroll", () =>{
+        mostrar()
+        hover()
+    });
     
 
   
