89e6ee736b9c639c5cf5408c78e7dbe17395cc92
Maiky Caires <122680022+MaikyCaires@users.noreply.github.com>
2024-05-16 14:54:26 -0300

    Update scrollDestinos.js

--- javaScript/module/scrollDestinos.js
+++ javaScript/module/scrollDestinos.js
@@ -1,27 +1,27 @@
-export default function scrollDestinos() {}
+export default function scrollDestinos() {
+  const container = document.querySelector('[data-container="destinations"]');
+  const card = document.querySelectorAll('[data-destinations="card"]');
 
-const container = document.querySelector('[data-container="destinations"]');
-const card = document.querySelectorAll('[data-destinations="card"]');
+  let alturaDisplay = window.innerHeight * 0.6;
 
-let alturaDisplay = window.innerHeight * 0.6;
-
-function mostrar(event) {
-  ajustarAltura(); // Atualiza a altura de exibição antes de verificar a visibilidade do contêiner
-  let rect = container.getBoundingClientRect().top;
-  if (rect <= alturaDisplay) {
-    card.forEach((elemento, indice) => {
-      setTimeout(() => {
-        elemento.classList.add("mostrarDestinations");
-      }, indice * 500);
-    });
+  function mostrar(event) {
+    let rect = container.getBoundingClientRect().top;
+    if (rect <= alturaDisplay) {
+      card.forEach((elemento, indice) => {
+        setTimeout(() => {
+          elemento.classList.add("mostrarDestinations");
+        }, indice * 500);
+      });
+    }
   }
-}
 
-function ajustarAltura() {
-  if (window.innerWidth > 800) {
-    window.addEventListener("resize", ajustarAltura); // Atualiza alturaDisplay quando a janela é redimensionada
-    window.addEventListener("scroll", () => {
-      mostrar();
-    });
+  function ajustarLargura() {
+    if (window.innerWidth > 800) {
+      window.addEventListener("scroll", () => {
+        mostrar();
+      });
+    }
   }
+  ajustarLargura();
+  window.addEventListener("resize", ajustarLargura);
 }
