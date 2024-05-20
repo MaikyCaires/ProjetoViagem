f81a611e256bba761d0adeffdf3cafb5a45bf202
Maiky Caires <122680022+MaikyCaires@users.noreply.github.com>
2024-05-20 10:54:52 -0300

    melhoria icone do botão

--- javaScript/module/slide.js
+++ javaScript/module/slide.js
@@ -1,31 +1,29 @@
-export default function slide(){
+export default function slide() {
+  const card = document.querySelectorAll('[data-destinations="card"]');
+  const botao = document.querySelector(".botao");
+  let contador = 0;
 
-}
+  card[contador].classList.add("on");
 
-const card = document.querySelectorAll('[data-destinations="card"]')
-const botao = document.querySelector(".botao")
-let contador = 0
+  function remover() {
+    card.forEach((element) => {
+      element.classList.remove("on");
+    });
+  }
 
-card[contador].classList.add("on")
-
-function remover(){
-    card.forEach(element =>{
-        element.classList.remove("on")
-    })
-}
-
-function slideMostrar(){
-    remover()
-    if(contador == card.length - 1){
-        contador = 0
-    }else{
-        contador ++
+  function slideMostrar() {
+    remover();
+    if (contador == card.length - 1) {
+      contador = 0;
+    } else {
+      contador++;
     }
-    
-    card[contador].classList.add("on")
-}
 
-botao.addEventListener("click", slideMostrar)
-card.forEach(element =>{
-    element.addEventListener("click", slideMostrar)
-})
+    card[contador].classList.add("on");
+  }
+
+  botao.addEventListener("click", slideMostrar);
+  card.forEach((element) => {
+    element.addEventListener("click", slideMostrar);
+  });
+}
