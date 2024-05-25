f86cd534823890c3c949aebdc5b85ed51bc62a00
Maiky Caires <122680022+MaikyCaires@users.noreply.github.com>
2024-05-25 00:20:10 -0300

    Função menu login / responsivo

--- javaScript/module/modal.js
+++ javaScript/module/modal.js
@@ -1,24 +1,45 @@
-export default function mostrarModal(){
-    
-}
+export default function mostrarModal() {
+  const btnCriar = document.querySelector('[data-btn="criar"]');
+  const btnLogin = document.querySelector('[data-btn="login"]');
+  const btnLoginRegistro = document.querySelector(
+    '[data-btn="login-registro"]'
+  );
 
-const btnLogin = document.querySelector('[data-btn="login"]')
-const containerModal = document.querySelector('[data-container="modal"]')
-const containerGeral = document.querySelector('[data-container="container"]');
+  const containerLogin = document.querySelector('[data-container="login"]');
+  const containerRegistro = document.querySelector(
+    '[data-container="registro"]'
+  );
 
-function mostrar(){
-    containerModal.classList.add("mostrarForm")
-}
+  const containerModal = document.querySelector('[data-container="modal"]');
+  const containerGeral = document.querySelector('[data-container="container"]');
 
-function clicarFora(event){
-    if(event.target == this){
-        containerModal.classList.remove("mostrarForm")
+  function mostrar(event) {
+    containerLogin.classList.remove("sumir");
+    containerRegistro.classList.remove("mostrarRegistro");
+    containerModal.classList.add("mostrarForm");
+  }
+  function clicarFora(event) {
+    if (event.target == this) {
+      containerModal.classList.remove("mostrarForm");
     }
+  }
+  function criarConta(event) {
+    event.preventDefault();
+    containerLogin.classList.add("sumir");
+    containerRegistro.classList.add("mostrarRegistro");
+  }
+
+  function fazerLogin(event) {
+    event.preventDefault();
+    containerRegistro.classList.remove("mostrarRegistro");
+    containerLogin.classList.remove("sumir");
+    containerLogin.classList.add("mostrarRegistro");
+  }
+
+  btnCriar.addEventListener("click", criarConta);
+  btnLoginRegistro.addEventListener("click", fazerLogin);
+
+  btnLogin.addEventListener("click", mostrar);
+
+  containerModal.addEventListener("click", clicarFora);
 }
-
-
-
-btnLogin.addEventListener("click", mostrar)
-
-
-containerModal.addEventListener("click", clicarFora)
